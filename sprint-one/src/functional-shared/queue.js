599390945088384d84b1fe30af159d5bf4100367
Chen Wilson <chenlwilson@gmail.com>
2018-10-07 22:12:28 -0700

    Complete functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.stackMax = 0;
+  someInstance.stackMin = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+//if using extend instead of _.extend
 
+// var extend = function(obj, methods) {
+//   for (var key in methods) {
+//     obj[key] = methods[key];
+//   }
+// }
 
+var queueMethods = {
+  size: function() {
+    if(this.stackMax - this.stackMin > 0) {
+      return this.stackMax - this.stackMin;
+    }
+    return 0;
+  },
+
+  enqueue: function(value) {
+    this.storage[this.stackMax] = value;
+    this.stackMax++;
+  },
+
+  dequeue: function() {
+    var firstItem = this.storage[this.stackMin];
+    delete this.storage[this.stackMin];
+    this.stackMin++;
+    return firstItem;
+  }
+
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.stackSize = 0;
 
-var stackMethods = {};
+  _.extend(someInstance, stackMethods);
 
+  return someInstance;
+}
 
+//if using extend instead of _.extend
+
+// var extend = function(obj, methods) {
+//   for (var key in methods) {
+//     obj[key] = methods[key];
+//   }
+// }
+
+var stackMethods = {
+  size: function() {
+    if(this.stackSize > 0) {
+      return this.stackSize;
+    }
+    return 0;
+  },
+
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop: function() {
+    this.stackSize--;
+    var lastItem = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return lastItem;
+  }
+
+}
