b67ae272fdaa67a378f29f5414c5ba988aec1229
Chen Wilson <chenlwilson@gmail.com>
2018-10-11 18:58:57 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,40 @@
 var LinkedList = function() {
+
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);//{value:value, next:null}
+    if(list.head === null && list.head === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    }
+    list.tail.next = newNode;//add new node to the lastnode.next
+    list.tail = newNode;//point tail to the new node
   };
 
   list.removeHead = function() {
+    list.head = list.head.next;//point head to the 2nd node
+    return list.head.value;
   };
 
   list.contains = function(target) {
+    var hasTarget = function(n) {
+      if (n.value === target) {
+        return true;
+      } else if (n.next) {
+        return hasTarget(n.next);
+      }
+      return false;
+    }
+
+    if (list.head.value === target) {
+      return true;
+    } else if (list.head.next) {
+      return hasTarget(list.head.next);
+    }
+    return false;
   };
 
   return list;
