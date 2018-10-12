b3b98d4425b968e7f79afe919ec4f20483d4711c
Chen Wilson <chenlwilson@gmail.com>
2018-10-11 18:18:43 -0700

    WIP

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,25 @@
-var BinarySearchTree = function(value) {
-  
-};
+class BinarySearchTree {
+  constructor(value, left, right) {
+    this.value = value;
+    this.left = [];
+    this.right = [];
+  }
 
+  insert(newValue) {
+    if(newValue < this.value) {
+      this.left.push(BinarySearchTree(newValue));
+    }
+    this.right.push(BinarySearchTree(newValue));
+  };
+
+  contains(target) {
+
+  };
+
+  depthFirstLog(cb) {
+
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
