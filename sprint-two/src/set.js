e2a41a3c15216f606e83cab554fe377d61504983
Chen Wilson <chenlwilson@gmail.com>
2018-10-11 20:04:13 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,20 +7,19 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this._storage.push(item)
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
-  if(this._storage.includes(item)) {
+  if (this._storage.includes(item)) {
     return true;
   }
   return false;
 };
 
 setPrototype.remove = function(item) {
-  this._storage.splice(this._storage.indexOf(item),1);
+  this._storage.splice(this._storage.indexOf(item), 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
