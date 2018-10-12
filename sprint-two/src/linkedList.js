06516ba3a82204c4111645e4a4f2fc450a46dba7
Chen Wilson <chenlwilson@gmail.com>
2018-10-11 18:57:57 -0700

    reviewed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,7 +5,7 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     var newNode = Node(value);//{value:value, next:null}
-    if(list.head === null) {
+    if(list.head === null && list.head === null) {
       list.tail = newNode;
       list.head = newNode;
     }
@@ -13,20 +13,52 @@ var LinkedList = function() {
     list.tail = newNode;//point tail to the new node
   };
 
+
+
+
   list.removeHead = function() {
     list.head = list.head.next;//point head to the 2nd node
     return list.head.value;
-
   };
 
   list.contains = function(target) {
     var hasTarget = function(n) {
-      if(n.value === target) {
+      if (n.value === target) {
         return true;
       } else if (n.next) {
         return hasTarget(n.next);
       }
       return false;
+    }
+
+    if (list.head.value === target) {
+      return true;
+    } else if (list.head.next) {
+      return hasTarget(list.head.next);
+    }
+    return false;
+  };
+
+  return list;
+};
+
+
+
+  var Node = function(value) {
+    var node = {};
+
+    node.value = value;//value: n
+    node.next = null;//next: node
+
+    return node;
+  };
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ * addToTail: O(1)
+ * removeHead: O(1)
+ * contains: O(n)
     }
 
     if(list.head.value === target) {
