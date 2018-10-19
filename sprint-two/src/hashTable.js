3ea3d62a945d712b224cb9919f59fd6f40e12e83
Chen Wilson <chenlwilson@gmail.com>
2018-10-18 22:59:21 -0700

    Started advanced part of Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,25 +3,29 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
   //[index: [[k,v][k2,v2]], index2: []]
 };
 
 HashTable.prototype.insert = function(k, v) {
+  //check the limit and current storage;
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (!bucket) {
-    bucket = [];
+    bucket = [[k, v]];
     this._storage.set(index, bucket);
-
-  }
-
-  for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      bucket[i].splice(1, 1, v);
+    this._count++;
+  } else if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i].splice(1, 1, v);
+      } else {
+        bucket.push([k, v]);
+        this._count++;
+      }
     }
   }
-  bucket.push([k, v]);
-
 
   if (bucket.length > 1) {
     return 'There is a conflict at index ' + index;
