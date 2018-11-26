f701b2b518a2b9d0c2ff1436fd9ab791d60ede2f
Chen Wilson <chenlwilson@gmail.com>
2018-11-25 22:22:26 -0800

    cleaned up doublyLinkedList

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -1,5 +1,4 @@
 var doublyLinkedList = function() {
-
   var list = {};
   list._head = null;
   list._tail = null;
@@ -9,26 +8,24 @@ var doublyLinkedList = function() {
     if (list._head === null && list._tail === null) {
       list._tail = newNode;
       list._head = newNode;
+      return null;
     } else {
       var oldTail = list._tail;
       oldTail.next = newNode;//add new node to the lastnode.next
       newNode.previous = oldTail;//point previous to tail;
       list._tail = newNode;//point tail to the new node
-    }
-    if (oldTail !== null) {
       return oldTail.value;
     }
   };
 
   list.removeTail = function() {
+    var oldTail = list._tail;
     if (list._tail === null) {
       return null;
     } else if (list._tail.previous === null) {
-      var oldTail = list._tail;
       list._tail = null;
       return oldTail.value;
     } else {
-      var oldTail = list._tail;
       list._tail = list._tail.previous;
       list._tail.next = null;
       return oldTail.value;
@@ -40,41 +37,39 @@ var doublyLinkedList = function() {
     if (list._head === null) {
       list._head = newNode;
     } else {
-      list._head.next = newNode;
+      list._head.previous = newNode;
+      newNode.next = list._head;
       list._head = newNode;
     }
   };
 
   list.removeHead = function() {
+    var oldHead = list._head;
     if (list._head === null) {
       return null;
     } else if (list._head.next === null) {
-      var oldHead = list._head;
       list._head = null;
       return oldHead.value;
     } else {
-      list._head = list._head.next;//point head to the 2nd node
-      return list._head.value;
+      list._head = oldHead.next;//point head to the 2nd node
+      list._head.previous = null;
+      return oldHead.value;
     }
   };
 
   list.contains = function(target) {
-    var hasTarget = function(n) {
-      if (n.value === target) {
+
+    var hasTarget = function(node) {
+      if (node.value === target) {
         return true;
-      } else if (n.next) {
-        return hasTarget(n.next);
+      } else if (node.next) {
+        return hasTarget(node.next);
       }
       return false;
     };
 
-    if (list._head.value === target) {
-      return true;
-    } else if (list._head.next) {
-      return hasTarget(list._head.next);
-    }
-    return false;
-  };
+    return hasTarget(list._head);
+  }
 
   return list;
 };
