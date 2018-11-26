108fb1aae39a27b21a44c2f0d18b24c2fe42957b
Chen Wilson <chenlwilson@gmail.com>
2018-11-25 20:29:22 -0800

    cleaned up set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,62 +1,45 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-<<<<<<< HEAD
+
   set._storage = {};
-||||||| merged common ancestors
-  set._storage = null; // fix me
-=======
-  set.storage = {}; // fix me
->>>>>>> part-one
+
   return set;
 };
 
-var setPrototype = {};
+var setPrototype = {
+  add: function(item) {
 
-setPrototype.add = function(item) {
-<<<<<<< HEAD
-  if (typeof item === 'object') {
-    this._storage[JSON.stringify(item)] = item;
-  } else {
-    this._storage[item] = item;
+    if (typeof item === 'object') {
+      this._storage[JSON.stringify(item)] = item;
+    } else {
+      this._storage[item] = item;
+    }
+    return Object.keys(this._storage).length;
+  },
+
+  contains: function(item) {
+
+    if (typeof item === 'object') {
+      item = JSON.stringify(item);
+    }
+    if (this._storage[item]) {
+      return true;
+    }
+    return false;
+
+  },
+
+  remove: function(item) {
+
+    if (typeof item === 'object') {
+      item = JSON.stringify(item);
+    }
+    delete this._storage[item];
   }
-  return Object.keys(this._storage).length;
-||||||| merged common ancestors
-=======
-  this.storage[item] = true;
->>>>>>> part-one
+
 };
 
-setPrototype.contains = function(item) {
-<<<<<<< HEAD
-  if (typeof item === 'object') {
-    item = JSON.stringify(item);
-  }
-  if (this._storage[item]) {
-    return true;
-  }
-  return false;
-||||||| merged common ancestors
-=======
-  if(this.storage[item]) {
-    return true;
-  }
-  return false;
->>>>>>> part-one
-};
 
-setPrototype.remove = function(item) {
-<<<<<<< HEAD
-  if (typeof item === 'object') {
-    item = JSON.stringify(item);
-  }
-  delete this._storage[item];
-||||||| merged common ancestors
-=======
-  if(this.storage[item]) {
-    delete this.storage[item];
-  }
->>>>>>> part-one
-};
 
 /*
  * Complexity: What is the time complexity of the above functions?
