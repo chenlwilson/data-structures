3755b3cf67dcd63799716c5d42c28ee1aeca4ec5
Chen Wilson <chenlwilson@gmail.com>
2018-11-25 22:05:03 -0800

    cleaned up binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,100 +1,64 @@
-<<<<<<< HEAD
 var BinarySearchTree = function(value) {
   var newBST = {};
-
-  newBST._value = value;
-  newBST._left = null;
-  newBST._right = null;
+  newBST.value = value;
+  newBST.left = null;
+  newBST.right = null;
 
   newBST.insert = function(newValue) {
     var newNode = BinarySearchTree(newValue);
 
-    var addNode = function(tree) {
-      if (newValue < tree._value) {
-        if (tree._left === null) {
-          tree._left = newNode;
+    var addNode = function (node, newNode) {
+      if (newNode.value < node.value) {
+        if (node.left === null) {
+          node.left = newNode;
         }
-        return addNode(tree._left);
+        return addNode(node.left, newNode);
       }
-
-      if (newValue > tree._value) {
-        if (tree._right === null) {
-          tree._right = newNode;
+      if (newNode.value > node.value) {
+        if (node.right === null) {
+          node.right = newNode;
         }
-        return addNode(tree._right);
+        return addNode(node.right, newNode);
       }
-    };
+    }
 
-    return addNode(newBST);
-
+    return addNode(newBST, newNode);
   };
 
   newBST.contains = function(target) {
 
-    var search = function(node) {
+      var search = function(node) {
+        if (target === node.value) {
+          return true;
+        } else if (target < node.value && node.left) {
+          return search(node.left);
+        } else if (target > node.value && node.right) {
+          return search(node.right);
+        }
+        return false;
+      };
 
-      if (target === node._value) {
-        return true;
-      } else if (target < node._value && node._left !== null) {
-        return search(node._left);
-      } else if (target > node._value && node._right !== null) {
-        return search(node._right);
-      }
-      return false;
+      return search(newBST);
     };
-
-    return search(newBST);
-  };
 
   newBST.depthFirstLog = function(cb) {
 
-    var callEachValue = function(node) {
-      //{value: 5, left: {value: 3, }}
-      cb(node._value);
+    var recurse = function(node) { //{value: 5, left: {value: 3, }}
+      cb(node.value);
 
-      if (node._left) {
-        callEachValue(node._left);
+      if (node.left) {
+        recurse(node.left);
       }
-      if (node._right) {
-        callEachValue(node._right);
+      if (node.right) {
+        recurse(node.right);
       }
-
     };
 
-    return callEachValue(newBST);
-
-  };
+    return recurse(newBST);
+  }
 
   return newBST;
 };
-||||||| merged common ancestors
-var BinarySearchTree = function(value) {
-};
-=======
-class BinarySearchTree {
-  constructor(value, left, right) {
-    this.value = value;
-    this.left = [];
-    this.right = [];
-  }
-
-  insert(newValue) {
-    if(newValue < this.value) {
-      this.left.push(BinarySearchTree(newValue));
-    }
-    this.right.push(BinarySearchTree(newValue));
-  };
-
-  contains(target) {
->>>>>>> part-one
-
-  };
-
-  depthFirstLog(cb) {
-
-  }
-};
-
 
 
 /*
