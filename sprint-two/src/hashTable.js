52d44ee1e7862d2a22710127ad4e6029df023524
Chen Wilson <chenlwilson@gmail.com>
2018-11-25 20:57:48 -0800

    cleaned up hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,24 +1,16 @@
-
-
 var HashTable = function() {
   this._limit = 8;
-<<<<<<< HEAD
-  this._storage = LimitedArray(this._limit);
-  this._count = 0;
-  //[index: [[k,v][k2,v2]], index2: []]
-||||||| merged common ancestors
-  this._storage = LimitedArray(this._limit);
-=======
-  this._storage = LimitedArray(this._limit);//8
->>>>>>> part-one
+
+  this._storage = LimitedArray(this._limit); //{};
+  this._count = 0; //[index: [[k,v][k2,v2]], index2: []]
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   //check the limit and current storage;
-
   var index = getIndexBelowMaxForKey(k, this._limit);
-<<<<<<< HEAD
   var bucket = this._storage.get(index);
+
   if (!bucket) {
     bucket = [[k, v]];
     this._storage.set(index, bucket);
@@ -38,16 +30,11 @@ HashTable.prototype.insert = function(k, v) {
     return 'There is a conflict at index ' + index;
   }
 
-||||||| merged common ancestors
-=======
-  return this._storage.set(index, v);
->>>>>>> part-one
 };
 
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-<<<<<<< HEAD
   var bucket = this._storage.get(index);
   if (!bucket) {
     return undefined;
@@ -57,40 +44,24 @@ HashTable.prototype.retrieve = function(k) {
       return bucket[i][1];
     }
   }
-||||||| merged common ancestors
-=======
-  return this._storage.get(index);
->>>>>>> part-one
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-<<<<<<< HEAD
   var bucket = this._storage.get(index);
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
       bucket.splice(i, 1);
     }
   }
-||||||| merged common ancestors
-=======
-  return this._storage.set(index, undefined);
->>>>>>> part-one
+
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
-<<<<<<< HEAD
  * insert: O(n)
  * retrieve: O(n)
  * remove: O(n)
 */
-||||||| merged common ancestors
- */
-
-
-=======
- */
->>>>>>> part-one
